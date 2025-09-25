fcbd52900973e5743a78399f419840aef6dc01fd
Ahmad Mohaddeszadeh <ahmad.mohaddeszadeh@gmail.com>
2025-09-26 00:15:34 +0200

    initial commit

---
+++ cdk/test/kanban.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as KanbanCdk from '../lib/kanban-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/kanban-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Kanban.KanbanStack(app, 'MyKanbanStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
