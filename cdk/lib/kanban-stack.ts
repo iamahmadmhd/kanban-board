493b5ba4d7ca1c7a7a3fe744ab1de9c822c4c081
Ahmad Mohaddeszadeh <ahmad.mohaddeszadeh@gmail.com>
2025-09-27 02:59:40 +0200

    feat(infra): dynamodb configuration

--- cdk/lib/kanban-stack.ts
+++ cdk/lib/kanban-stack.ts
@@ -1,16 +1,55 @@
 import * as cdk from 'aws-cdk-lib';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class KanbanStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    public readonly table: dynamodb.Table;
 
-    // The code that defines your stack goes here
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'KanbanQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        // DynamoDB table with single-table design
+        this.table = new dynamodb.Table(this, 'KanbanTable', {
+            tableName: 'KanbanTable',
+            partitionKey: {
+                name: 'PK',
+                type: dynamodb.AttributeType.STRING,
+            },
+            sortKey: {
+                name: 'SK',
+                type: dynamodb.AttributeType.STRING,
+            },
+            billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+            removalPolicy: cdk.RemovalPolicy.DESTROY, // For development
+            pointInTimeRecovery: false, // Keep costs down
+            encryption: dynamodb.TableEncryption.AWS_MANAGED,
+        });
+
+        // GSI for querying boards by user
+        this.table.addGlobalSecondaryIndex({
+            indexName: 'GSI1',
+            partitionKey: {
+                name: 'GSI1PK',
+                type: dynamodb.AttributeType.STRING,
+            },
+            sortKey: {
+                name: 'GSI1SK',
+                type: dynamodb.AttributeType.STRING,
+            },
+        });
+
+        // Output table name for frontend integration
+        new cdk.CfnOutput(this, 'TableName', {
+            value: this.table.tableName,
+            description: 'DynamoDB table name',
+            exportName: 'KanbanTableName',
+        });
+
+        // Output table ARN for Lambda permissions
+        new cdk.CfnOutput(this, 'TableArn', {
+            value: this.table.tableArn,
+            description: 'DynamoDB table ARN',
+            exportName: 'KanbanTableArn',
+        });
+    }
 }
